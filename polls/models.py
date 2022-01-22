ad379a7948b0fddb0ea4fdc893ff3ce2b2f401e7
Patricio Requena <rprequenac@gmail.com>
2022-01-22 14:07:52 -0500

    First migrations for polls app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    """
+    Model for Questions table in our DB
+    """
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    """
+    Model for choices based on the Question table / model
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
